253b2480911f97194e30a98409a39dafb7aa7ff4
Максим Попков <your@email.com>
2026-04-03 17:36:08 +0300

    Спринт 1: Исправлены маршруты для shop

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('apps.shop.urls')),
+    path('api/shop/', include('apps.shop.urls')),
 ]
