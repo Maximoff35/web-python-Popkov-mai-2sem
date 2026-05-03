c6095d266dce4a0c1c781095524dc995e7671e22
Максим Попков <your@email.com>
2026-05-03 15:16:22 +0300

    Спринт 3: Исправлено created_at в Review

--- ugc_service/models.py
+++ ugc_service/models.py
@@ -19,7 +19,7 @@ class Review(db.Model):
     text = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     status = db.Column(db.String(20), nullable=False, default=STATUS_PENDING)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(UTC))
+    created_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(UTC))
 
     def to_dict(self):
         return {
